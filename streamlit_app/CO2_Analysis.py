12de98fd29e55a5c67a1b8d934089b685d6bd06e
A. S. Barde <ahmadsanibarde@gmail.com>
2022-07-19 11:50:05 +0100

    Update CO2_Analysis.py

--- streamlit_app/CO2_Analysis.py
+++ streamlit_app/CO2_Analysis.py
@@ -26,7 +26,7 @@ def main():
         # Header contents
         st.write('# CO2 Emission Analysis')
         st.write('### Ai Glass Data Science Team')
-        st.image('imgs/teddy.jpeg',use_column_width=True)
+        st.image('streamlit_app/imgs/teddy.jpeg',use_column_width=True)
 
 
 
