0a6d79ac15df584b669b4c8cc3d18a2b0782dcb5
A. S. Barde <ahmadsanibarde@gmail.com>
2022-07-19 11:57:26 +0100

    Update CO2_Analysis.py

--- streamlit_app/CO2_Analysis.py
+++ streamlit_app/CO2_Analysis.py
@@ -27,7 +27,16 @@ def main():
         st.write('# CO2 Emission Analysis')
         st.write('## Ai Glass Data Science Team')
         st.write('### Meet The Team')
-        st.image('streamlit_app/imgs/teddy.jpeg',use_column_width=False, caption = 'teddy')
+        st.image('streamlit_app/imgs/teddy.jpeg',use_column_width=True, caption = 'Teddy')
+        st.image('streamlit_app/imgs/ahmad.jpeg',use_column_width=True, caption = 'Ahmad')
+        st.image('streamlit_app/imgs/raph.jpeg',use_column_width=True, caption = 'Raphael')
+        st.image('streamlit_app/imgs/ebere.jpeg',use_column_width=True, caption = 'Ebere')
+        st.image('streamlit_app/imgs/dare.jpeg',use_column_width=True, caption = 'Dare')
+        st.image('streamlit_app/imgs/teddy.jpeg',use_column_width=True, caption = 'Israel')
+
+        st.write('## Problem Statement')
+
+        st.write('## Overview of Our Solution')
 
 
 
@@ -37,8 +46,9 @@ def main():
     
     if page_selection == "Data Analysis":
         # Header contents
-        st.write('# Movie Recommender Engine')
-        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
+        st.write('# CO2 Emission Analysis')
+        st.write('## Data Analysis')
+        st.write('### Insights of the Data')
         # st.image('resources/imgs/Image_header.png',use_column_width=True)
 
     # -------------------------------------------------------------------
@@ -47,8 +57,8 @@ def main():
     
     if page_selection == "Model Deployment":
         # Header contents
-        st.write('# Movie Recommender Engine')
-        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
+        st.write('# CO2 Emission Analysis')
+        st.write('## Model Deployment')
         # st.image('resources/imgs/Image_header.png',use_column_width=True)
 
     
@@ -58,8 +68,8 @@ def main():
     
     if page_selection == "Conclusion":
         # Header contents
-        st.write('# Movie Recommender Engine')
-        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
+        st.write('# CO2 Emission Analysis')
+        st.write('## Conclusion')
         # st.image('resources/imgs/Image_header.png',use_column_width=True)
     
 
